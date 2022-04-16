e1248b58aef5a6599d652ecdbfd71d60e8a6c017
Sherla yualinda <43510133+sherlayualinda@users.noreply.github.com>
2022-04-16 12:44:36 +0700

    navbar

--- resources/views/lahan.blade.php
+++ resources/views/lahan.blade.php
@@ -1,41 +1,8 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
+@include('nav_barMar')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Sosial Media Desaku</title>
-	<link rel="icon" href="/logo-home.png" type="image/png" sizes="16x16"> 
-	    
-    <link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/main.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/color.css') }}">
-    <link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/responsive.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('slick-1.8.1/slick/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('slick-1.8.1/slick/slick-theme.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('jquery-ui-1.12.1.custom/jquery-ui.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/sweetalert2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/slideshow.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/read-less-more.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/halaman-beranda.css') }}">
-    <!-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}"> -->
-
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/> -->
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/> -->
-
-</head>
-<body id="bodies" style="overflow-y: auto;">
-<!--<div class="se-pre-con"></div>-->
-<div class="theme-layout">
-	
-	
-	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-		@include('nav_barMar')
-	</nav>
-=======
-ini lahan
->>>>>>> Stashed changes
+<link rel="icon" href="/logo-home.png" type="image/png" sizes="16x16"> 	    
+<link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/main.min.css') }}">
--- resources/views/nav_barMar.blade.php
+++ resources/views/nav_barMar.blade.php
@@ -1,51 +1,115 @@
-<style type="text/css">
-.navbar .megamenu{ padding: 1rem; }
-/* ============ desktop view ============ */
-@media all and (min-width: 992px) {
-	
-	.navbar .has-megamenu{position:static!important;}
-	.navbar .megamenu{margin-left: 150px; width:50%; margin-top:0;  }
-	
-}	
-/* ============ desktop view .end// ============ */
+<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+	<div class="logo col-lg-2">
+		<a title="" href="{{ asset('/sosial-media/beranda') }}">
+			<img src="/Diessnie_logo.jpeg" alt="" style="max-height: 50px;">
+		</a>
+	</div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-
-/* ============ mobile view ============ */
-@media(max-width: 991px){
-	.navbar.fixed-top .navbar-collapse, .navbar.sticky-top .navbar-collapse{
-		overflow-y: auto;
-	    max-height: 90vh;
-	    margin-top:10px;
-	}
-}
-
-@media all and (min-width: 992px) {
-	.navbar .dropdown-menu-end{ right:0; left: auto;  }
-	.navbar .nav-item .dropdown-menu{  display:block; opacity: 0;  visibility: hidden; transition:.3s; margin-top:0;  }
-	.navbar .nav-item:hover .nav-link{ color: black;  }
-	.navbar .dropdown-menu.fade-down{ top:80%; transform: rotateX(-75deg); transform-origin: 0% 0%; }
-	.navbar .dropdown-menu.fade-up{ top:180%;  }
-	.navbar .nav-item:hover .dropdown-menu{ transition: .3s; opacity:1; visibility:visible; top:100%; transform: rotateX(0deg); }
-}
-/* ============ mobile view .end// ============ */
-</style>
-
-<script type="text/javascript">
-	document.addEventListener("DOMContentLoaded", function(){
-        /////// Prevent closing from click inside dropdown
-        document.querySelectorAll('.dropdown-menu').forEach(function(element){
-        	element.addEventListener('click', function (e) {
-        		e.stopPropagation();
-        	});
-        })
-    }); 
-	// DOMContentLoaded  end
-</script>
-
-<nav nav class="navbar navbar-dark bg-primary">
-  <a class="navbar-brand">Navbar</a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+  	<ul class="navbar-nav mr-auto">
+	  <li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle menu-text" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+				Produk Desaku
+				</a>
+				<div class="dropdown-menu box-size" aria-labelledby="navbarDropdown" style="width: 850px!important;">
+					<div class="row" style="margin-right: 0; margin-left: 0;">
+						<div class="col desa-col">
+							@if(Auth::check())
+							<a href="{{ url('sosial-media/beranda') }}" class="dropdown-item">
+								<img src="/desafeed-logo.png" style="min-width: 150px;">
+								<br>
+								<small style="white-space: normal!important;">Media Sosial Untuk Kamu</small>
+							</a>
+								<form id="login-form-store" action="#" method="POST" class="d-none">
+									</form>
+							@else
+							<a class="dropdown-item" target="_blank" href="{{ url(sosial-media/beranda) }}">
+								<img src="/desafeed-logo.png" style="min-width: 150px;">
+								<br>
+								<small style="white-space: normal!important;">Jual & beli berbagai produk desa di DesaStore!</small>
+							</a>
+							@endif
+						</div>
+					</div>
+					<div class="row" style="margin-right: 0; margin-left: 0;">
+						<div class="col desa-col">
+						    @if(Auth::check())
+							<a href="{{ url('/lahan') }}" class="dropdown-item">
+							    <img src="/desatour-logo.png" style="min-width: 150px;">
+								<br>
+								<small style="white-space: normal!important;">Jelajahi wisata, kuliner, penginapan, dan infrastruktur desa di DesaTour!</small>
+							</a>
+                            <form id="login-form-tour" action="{{ url('/lahan') }}" method="POST" class="d-none">
+                                <input type="hidden" name="email" value="{{ auth()->user()->pengguna->email }}">
+                                <input type="hidden" name="id_desa" value="{{ auth()->user()->pengguna->village_id }}">
+                            </form>
+							@else
+							<a class="dropdown-item" target="_blank" href="https://desaku-desatour.masuk.id">
+								<img src="/desatour-logo.png" style="min-width: 150px;">
+								<br>
+								<small style="white-space: normal!important;">Jelajahi wisata, kuliner, penginapan, dan infrastruktur desa di DesaTour!</small>
+							</a>
+							@endif
+						</div>
+					</div>
+					<div class="row" style="margin-right: 0; margin-left: 0;">
+						<div class="col desa-col">
+						    @if(Auth::check())
+							<a href="{{ url('categories') }}" class="dropdown-item">
+							    <img src="/desastore-logo.png" style="min-width: 150px;">
+								<br>
+								<small style="white-space: normal!important;">Jual & beli berbagai produk desa di DesaStore!</small>
+							</a>
+                            <form id="login-form-store" action="#" method="POST" class="d-none">
+                                <!-- <input type="hidden" name="email" value="{{ auth()->user()->pengguna->email }}">
+                                <input type="hidden" name="id_desa" value="{{ auth()->user()->pengguna->village_id }}"> -->
+                            </form>
+							@else
+							<a class="dropdown-item" target="_blank" href="{{ url(sosial-media/marketplace) }}">
+								<img src="/desastore-logo.png" style="min-width: 150px;">
+								<br>
+								<small style="white-space: normal!important;">Jual & beli berbagai produk desa di DesaStore!</small>
+							</a>
+							@endif
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="mx-auto" style="width: 200px;">
+				
+				</div>
+			</li>
+			<li>
+				<form class="form-inline my-2 my-lg-0">
+				  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				  <button class="btn btn-secondary my-2 my-sm-0" type="submit" >Search</button>
+				</form>
+			</li>
+			
+		</ul>
+		<ul class="navbar-nav ml-auto text-center">
+			<li class="nav-item dropdown ">
+					<a class="nav-link dropdown-toggle img_nav prof" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img src="{{ auth()->user()->pengguna->foto_profil != null ? url('/data_file/'.auth()->user()->pengguna->username.'/foto_profil/'.auth()->user()->pengguna->foto_profil) : asset('user.jpg') }}" alt="" style="height: 20px;width:20px;border-radius:50%;vertical-align:sub;">
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#" title="">{{ auth()->user()->name }}</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="/sosial-media/profil/{{auth()->user()->pengguna->username}}" title=""><i class="ti-user" style="padding-right: 1rem;"></i>Profil</a>
+						<!-- <a class="dropdown-item" href="" title=""><i class="fa fa-bar-chart-o" style="padding-right: 1rem;"></i>Insight</a> -->
+						<a class="dropdown-item" href="/sosial-media/pengaturan" title=""><i class="ti-settings" style="padding-right: 1rem;"></i>Pengaturan</a>
+						{{-- <a class="dropdown-item" href="/sosial-media/logout_proses" title=""><i class="ti-power-off" style="padding-right: 1rem;"></i>Logout</a> --}}
+						<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="ti-power-off" style="padding-right: 1rem;"></i> Logout </a>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							{{ csrf_field() }}
+						</form>
+					</div>
+				</li>
+		</ul>
+	</div>
 </nav>
