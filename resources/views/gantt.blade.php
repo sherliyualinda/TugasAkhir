b10294804ad2e9124f09f731ab3e6690fc377d4c
Sherla yualinda <43510133+sherlayualinda@users.noreply.github.com>
2022-04-16 14:41:38 +0700

    update gantt

--- resources/views/gantt.blade.php
+++ resources/views/gantt.blade.php
@@ -1,36 +1,11 @@
-<!-- <!DOCTYPE html>
-<head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+@include('nav_barMar')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js"></script>
-    <link href="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.css" rel="stylesheet">
-
-    <style type="text/css">
-        html, body{
-            height:100%;
-            padding:0px;
-            margin:0px;
-            overflow: hidden;
-        }
-    </style>
-</head>
-<body>
-<div id="gantt_here" style='width:100%; height:100%;'></div>
-<script type="text/javascript">
-    gantt.config.xml_date = "%Y-%m-%d %H:%i:%s";
-    gantt.config.order_branch = true;/*!*/
-    gantt.config.order_branch_free = true;/*!*/
-    gantt.init("gantt_here");
-
-    gantt.load("/api/data");
-
-    var dp = new gantt.dataProcessor("/api");/*!*/
-    dp.init(gantt);/*!*/
-    dp.setTransactionMode("REST");/*!*/
-</script>
-</body> -->
-
-
+<link rel="icon" href="/logo-home.png" type="image/png" sizes="16x16"> 	    
+<link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/main.min.css') }}">
 
 
 <!DOCTYPE html>
