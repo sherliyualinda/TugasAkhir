aebfd8e5014ee875e5f142cde222b0fbf6e789cb
Sherla yualinda <43510133+sherlayualinda@users.noreply.github.com>
2022-04-19 11:55:28 +0700

    ff

--- resources/views/Create_lahan.blade.php
+++ resources/views/Create_lahan.blade.php
@@ -1,3 +1,11 @@
+@include('nav_barMar')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<link rel="icon" href="/logo-home.png" type="image/png" sizes="16x16"> 	    
+<link rel="stylesheet" href="{{ asset('Winku-Social-Network-Corporate-Responsive-Template/css/main.min.css') }}">
 
 <div class="limiter">
     <div class="container-login100">
@@ -16,7 +24,7 @@
             @foreach($categori as $categori)
                 <option value="{{$categori->id}}">{{$categori->nama}}</option>
             @endforeach
-    </select>
+         </select>
     </tr>
   </div>
   <div class="form-group">
@@ -44,3 +52,6 @@
     </tr> 
   </div>
 </table>
+
+
+<!-- Percobaan -->
