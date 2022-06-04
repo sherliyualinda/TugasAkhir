811d099b37bdfa0de1b0aeb5856f730aac154bf6
Sherla yualinda <43510133+sherlayualinda@users.noreply.github.com>
2022-06-04 15:16:17 +0700

    Update GanttController.php

--- app/Http/Controllers/GanttController.php
+++ app/Http/Controllers/GanttController.php
@@ -7,11 +7,12 @@ use App\Lahan;
 class GanttController extends Controller
 {
     public function get(){
+        session_start();
         $tasks = new Task();
         $links = new Link();
  
         return response()->json([
-            "data" => $tasks->orderBy('sortorder')->get(),
+            "data" => $tasks->orderBy('sortorder')->where('id_lahan', $_SESSION['id_lahan'] )->get(),
             "links" => $links->all()
         ]);
     }
